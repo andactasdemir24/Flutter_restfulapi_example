9e05d2a82a65c4eec6c9ba3062f1b2222ccd9a88
andactasdemir24 <96003255+andactasdemir24@users.noreply.github.com>
2023-03-23 17:45:02 +0300

    Change title

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -32,7 +32,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Center(child: Text('Accounts')),
+      ),
       body: isLoading == false
           ? const Center(
               child: CircularProgressIndicator(),
